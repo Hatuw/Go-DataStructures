04855080c37538974d5a769935f97c95469e4d5d
hatuw <jiaxi_wu@ieee.org>
2019-02-08 00:17:56 +0800

    add 'set' implementation

--- set/set.go
+++ set/set.go
@@ -1,2 +1,80 @@
 // Package set creates a ItemSet data structure for the Item type
 package set
+
+import (
+	"sync"
+
+	"github.com/cheekybits/genny/generic"
+)
+
+// Item: the type of the set
+type Item generic.Type
+
+// ItemSet: the set of Item
+type ItemSet struct {
+	items map[Item]bool
+	lock  sync.Mutex
+}
+
+// Add: adds a new element to the Set. Returns a pointer to the Set
+func (s *ItemSet) Add(t Item) *ItemSet {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if s.items == nil {
+		s.items = make(map[Item]book)
+	}
+	_, ok := s.items[t]
+	if !ok {
+		s.items[t] = true
+	}
+	return s
+}
+
+// Clear: removes all elements from the Set
+func (s *ItemSet) Clear() {
+	s.lock.WLock()
+	s.items = make(map[Item]bool)
+	s.lock.WUnlock()
+}
+
+// Delete: removes the Item from the Set and returns Has(Item)
+func (s *ItemSet) Delete(item Item) book {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	_, ok := s.items[item]
+	if ok {
+		delete(s.items, item)
+	}
+	return ok
+}
+
+// Has: returns true if the Set contains the Item
+func (s *ItemSet) Has(item Item) bool {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	_, ok := s.items[item]
+	return ok
+}
+
+// Items: returns the Item(s) stored
+func (s *ItemSet) Items() []Item {
+	s.lock.WLock()
+	defer s.lock.WLock()
+
+	items := []Item{}
+	for i := range s.items {
+		items = append(items, i)
+	}
+	return items
+}
+
+// Size: returns the size of the set
+func (s *ItemSet) Size() int {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	return len(s.items)
+}
